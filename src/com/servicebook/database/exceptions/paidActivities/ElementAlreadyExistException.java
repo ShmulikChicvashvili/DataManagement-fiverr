03327edc506291bed7e816313a94d9ecc2b2be4c
eyalt <eyaltolchinsky@gmail.com>
2015-03-18 17:11:55 +0200

    no message

--- src/com/servicebook/database/exceptions/paidActivities/ElementAlreadyExistException.java
+++ src/com/servicebook/database/exceptions/paidActivities/ElementAlreadyExistException.java
@@ -2,14 +2,14 @@ package com.servicebook.database.exceptions.paidActivities;
 
 import com.servicebook.database.exceptions.AbstractDatabaseException;
 
-public class DatabaseAlreadyExistsException extends AbstractDatabaseException {
+public class ElementAlreadyExistException extends AbstractDatabaseException {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public DatabaseAlreadyExistsException(Exception e) {
+	public ElementAlreadyExistException(Exception e) {
 		super(e);
 		// TODO Auto-generated constructor stub
 	}
--- src/com/servicebook/database/exceptions/paidActivities/ElementNotExistException.java
+++ src/com/servicebook/database/exceptions/paidActivities/ElementNotExistException.java
@@ -1,7 +1,17 @@
 package com.servicebook.database.exceptions.paidActivities;
 
-import com.servicebook.database.AbstractMySqlDatabase;
+import com.servicebook.database.exceptions.AbstractDatabaseException;
 
-public class ElementNotExistException extends AbstractMySqlDatabase {
+public class ElementNotExistException extends AbstractDatabaseException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ElementNotExistException(Exception e) {
+		super(e);
+		// TODO Auto-generated constructor stub
+	}
 
 }
