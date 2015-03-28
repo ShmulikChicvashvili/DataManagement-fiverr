a7ff71e219e0d639775afec5ee7e0197f1afe2e7
Eyal Tolch <eyaltolchinsky@gmail.com>
2015-03-28 17:10:07 +0300

    Srsly don't know

--- src/com/servicebook/database/primitives/DBPaidService.java
+++ src/com/servicebook/database/primitives/DBPaidService.java
@@ -1,18 +1,35 @@
+
 package com.servicebook.database.primitives;
 
-public class DBPaidService extends DBPaidActivity {
 
-	public DBPaidService(String title, String username, int capacity,
-			int distance, int numRegistered) {
-		super(title, username, (short) capacity, (short) distance,
-				(short) numRegistered);
-		// TODO Auto-generated constructor stub
-	}
-
-	public DBPaidService(int id, String title, String username, short capacity,
-			short distance, short numRegistered) {
+public class DBPaidService extends DBPaidActivity
+{
+	
+	public DBPaidService(
+		int id,
+		String title,
+		String username,
+		short capacity,
+		short distance,
+		short numRegistered)
+	{
 		super(id, title, username, capacity, distance, numRegistered);
-		// TODO Auto-generated constructor stub
 	}
-
+	
+	
+	public DBPaidService(
+		String title,
+		String username,
+		int capacity,
+		int distance,
+		int numRegistered)
+	{
+		super(
+			title,
+			username,
+			(short) capacity,
+			(short) distance,
+			(short) numRegistered);
+	}
+	
 }
