f6026926f37ea5e28b6a5e955d95551c81c737ec
eyalt <eyaltolchinsky@gmail.com>
2015-04-12 20:07:32 +0300

    Added the XSLT advanced feature to dbXML servlet

---
+++ resources/friends.xsl
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0"
+	xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+	<xsl:output method="xml"></xsl:output>
+	<xsl:template match="/">
+		<graphml xmlns="http://graphml.graphdrawing.org/xmlns"
+			xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+			xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns
+     http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">
+
+			<key id="username" for="node" attr.name="username" attr.type="string"/>
+			<key id="name" for="node" attr.name="name" attr.type="string"/>
+			<key id="balance" for="node" attr.name="balance" attr.type="int"/>
+
+			<graph id="G" edgedefault="undirected">
+				<xsl:apply-templates
+					select="//user[username=//friendship[username=//currUser]/username[.!=//currUser]]" />
+
+			</graph>
+		</graphml>
+	</xsl:template>
+
+	<xsl:template match="user">
+		<node id="{username}">
+			<data id="username">
+				<xsl:value-of select="username"></xsl:value-of>
+			</data>
+			<data id="name">
+				<xsl:value-of select="name"></xsl:value-of>
+			</data>
+			<data id="balance">
+				<xsl:value-of select="balance"></xsl:value-of>
+			</data>
+		</node>
+	</xsl:template>
+</xsl:stylesheet>
--- src/servlets/DbXML.java
+++ src/servlets/DbXML.java
@@ -1,12 +1,21 @@
 package servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletContext;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.apache.tomcat.util.http.ContentType;
 import org.w3c.dom.Document;
@@ -37,11 +46,40 @@ public class DbXML extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws IOException {
+		String username = request.getParameter("username");
+		String typeStr = request.getParameter("type");
+		if (username == null || typeStr == null) {
+			// TODO error
+			return;
+		}
+		int type = 0;
+		try {
+			type = new Integer(typeStr);
+		} catch (NumberFormatException e) {
+			// TODO type is not a number
+			return;
+		}
+
+		String xslPath = null;
+		switch (type) {
+		case 1:
+			xslPath = "/users.xsl";
+			break;
+		case 2:
+			xslPath = "/friends.xsl";
+			break;
+		case 3:
+			xslPath = "/offeringUsers.xsl";
+			break;
+		default:
+			// TODO return invalid parameter
+			return;
+		}
+
 		ServletContext sc = getServletContext();
 		MultiTableDatabase multiDB = (MultiTableDatabase) sc
 				.getAttribute("multiTableDB");
 
-		String username = (String) request.getParameter("username");
 		Document doc = null;
 		try {
 			doc = multiDB.toXML(username);
@@ -49,6 +87,29 @@ public class DbXML extends HttpServlet {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		Transformer transformer = null;
+		try (PrintWriter out = response.getWriter()) {
+			transformer = TransformerFactory.newInstance().newTransformer(
+					new javax.xml.transform.stream.StreamSource(getClass()
+							.getResourceAsStream(xslPath)));
+			transformer.transform(new DOMSource(doc), new StreamResult(out));
+			response.setContentType("application/xml");
+
+			// just for debugging
+			DOMResult domResult = new DOMResult();
+			transformer.transform(new DOMSource(doc), domResult);
+			StreamResult result = new StreamResult(System.out);
+			System.out.println("dbXML after using XSL: " + xslPath);
+			transformer.transform(new DOMSource(domResult.getNode()), result);
+			System.out.println("");
+		} catch (TransformerConfigurationException
+				| TransformerFactoryConfigurationError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
